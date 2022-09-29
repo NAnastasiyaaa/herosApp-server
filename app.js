7d58880a94ee3293b1a078731c0a6458f7f28d16
NAnastasiyaaa <maherus16@gmail.com>
2022-09-29 18:59:41 +0300

    add fixed

--- app.js
+++ app.js
@@ -2,45 +2,45 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const router = require("./routes/hero-routes");
-const cors=require('cors');
+const cors = require("cors");
 //
-const multer = require('multer');
+const multer = require("multer");
 //
 
 app.use(express.json());
 app.use(cors());
 app.use("/heros", router);
 //
-app.use(express.static('public'));
+app.use(express.static("public"));
 //
 
 /////
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, 'public')
+    cb(null, "public");
   },
   filename: (req, file, cb) => {
-      cb(null, Date.now() + '-' + file.originalname)
-  }
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-const upload = multer({storage}).array('file');
+const upload = multer({ storage }).array("file");
 
-app.post('/upload', (req, res) => {
+app.post("/upload", (req, res) => {
   upload(req, res, (err) => {
-      if (err) {
-          return res.status(500).json(err)
-      }
-      return res.status(200).send(req.files)
-  })
+    if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.files);
+  });
 });
 /////
 
-
 mongoose
-  .connect(
-    "mongodb+srv://admin:Nastyatrivoga2022@cluster0.kx7kn.mongodb.net/heroStore?retryWrites=true&w=majority"
-  )
+.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log("Connected to DB"))
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
